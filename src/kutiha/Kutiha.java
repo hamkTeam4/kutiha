c9911c1c97a3a61f851428d8704ed1dc90b7996b
Rockd11 <35866801+Rockd11@users.noreply.github.com>
2018-02-28 19:08:36 +0200

    Version nosto 0.9

--- src/kutiha/Kutiha.java
+++ src/kutiha/Kutiha.java
@@ -5,9 +5,14 @@
  */
 package kutiha;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 /**
@@ -20,8 +25,9 @@ public class Kutiha {
      * @param args the command line arguments
      * @throws java.sql.SQLException
      * @throws java.io.FileNotFoundException
+     * @throws java.text.ParseException
      */
-    public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
+    public static void main(String[] args) throws SQLException, FileNotFoundException, IOException, ParseException {
 
         try {
             // The newInstance() call is a work around for some
@@ -35,7 +41,7 @@ public class Kutiha {
         System.out.println("*********************************************");
         System.out.println("*                                           *");
         System.out.println("* KULUNVALVONTATIETOKANNAN HALLINTASOVELLUS *");
-        System.out.println("*                KUTIHA 0.7                 *");
+        System.out.println("*                KUTIHA 0.8.1                 *");
         System.out.println("*                                           *");
         System.out.println("*********************************************");
 
@@ -51,6 +57,8 @@ public class Kutiha {
         Scanner scanner2 = new Scanner(System.in);
         Scanner scanner3 = new Scanner(System.in);
         Scanner scanner4 = new Scanner(System.in);
+        Scanner scanner5 = new Scanner(System.in);
+        Scanner scanner6 = new Scanner(System.in);
         //Scanner scanner1 = new Scanner(System.in);
         kysely k = new kysely();
         int valinta;
@@ -59,6 +67,8 @@ public class Kutiha {
         int hakuID;
         String hakuNimi;
         int hakuEvent;
+        String hakuOvi;
+        String hakuAika;
 
         do {
             System.out.println("_____________________________________________");
@@ -67,7 +77,10 @@ public class Kutiha {
             System.out.println("2. Tapahtumahaku henkilötunnisteen mukaan");
             System.out.println("3. Tapahtumahaku henkilön mukaan");
             System.out.println("4. Tapahtumahaku tapahtumaluokan mukaan");
-            System.out.println("5. Poistu");
+            System.out.println("5. Tapahtumahaku oven mukaan");
+            //System.out.println("6. Haku päivämäärän mukaan");
+            System.out.println("6. Poistu");
+            System.out.println("7. README.md");
             System.out.println("_____________________________________________\n");
             System.out.print("Anna valinta: ");
             valinta = scanner.nextInt();
@@ -99,9 +112,46 @@ public class Kutiha {
                     k.kyselyTapahtumatByTapahtuma(hakuEvent);
                     break;
                 case 5:
+                    System.out.println("OVET:");
+                    System.out.println("U1 = PÄÄSISÄÄNKÄYNTI (ULKO-OVI)");
+                    System.out.println("U2 = SISÄÄNKÄYNTI TOIMISTOT 2 (ULKO-OVI)");
+                    System.out.println("U3 = SISÄÄNKÄYNTI VARASTO (ULKO-OVI)");
+                    System.out.println("S1 = SISÄOVI (AULA <-> TOIMISTOT1)");
+                    System.out.println("S2 = SISÄOVI (TOIMISTOT 1 <-> TOIMISTOT 2)");
+                    System.out.println("S3 = SISÄOVI (TOIMISTOT 2 <-> VARASTO)");
+                    System.out.println("S4 = SISÄOVI (TOIMISTOT 1 <-> VARASTO)");
+                    System.out.println("\nSyötä ovi:");
+                    hakuOvi = scanner5.nextLine();
+                    k.kyselyTapahtumatByOvi(hakuOvi);
+                    break;
+                /*case 6:
+                    System.out.println("\nSyötä haettavan päivän tapahtumat:");
+                    System.out.println("Syötä aika muodossa vvvv-kk-pp");
+                    hakuAika = scanner6.nextLine();
+                    k.kyselyTapahtumatByAika(hakuAika);
+                    break;*/
+                case 6:
                     // quit.
                     System.out.println("Olet poistunut sovelluksesta - TURVALLISESTI.");
                     quit = true;
+                    break;
+                case 7:
+                    try {
+                        // Printtaa readme.md:n projektin kansiosta                      
+                        FileInputStream fstream = new FileInputStream("README.md");
+                        // uudet oliot
+                        DataInputStream in = new DataInputStream(fstream);
+                        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                        String strLine;
+                        //Rivi riviltä tekstitiedoston lukeminen
+                        while ((strLine = br.readLine()) != null) {
+                            System.out.println(strLine);
+                        }
+                        //inputti kiinni
+                        in.close();
+                    } catch (Exception e) {
+                        System.err.println("Error: " + e.getMessage());
+                    }
                     break;
                 default:
                     for (int i = 0; i < 50; ++i) {
@@ -109,6 +159,7 @@ public class Kutiha {
                     }
                     System.out.println("Tuntematon valinta");
             }
+
         } while (!quit);
     }
 }
